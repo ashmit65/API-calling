37599ed0da94080c0302e0eeaabbbc5eb05e453d
ashmit <ashmits554@gmail.com>
2025-05-09 14:22:52 +0530

    Initial commit

---
+++ app/Contact/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold '>This is Home LAAN bHEco</h1>  
+      
+    </div>
+  )
+}
+
+export default contact
